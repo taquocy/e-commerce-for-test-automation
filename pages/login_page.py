7a5d6d4ecec0a1c7491edeabe3beffe37e4ac68b
Phan Anh Phuong <phuong95646@donga.edu.vn>
2024-11-01 14:40:04 +0700

    XPATH page login

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,8 +7,8 @@ class LoginPage:
 
         # Xác định các phần tử trên trang login
         self.menu_login =  (By.XPATH, "//a[@href='/signin']/button")
-        self.username_input = (By.NAME, "email")  # Tìm trường username
-        self.password_input = (By.NAME, "password")  # Tìm trường password
+        self.username_input = (By.XPATH, "//input[@name='email']")  # Tìm trường username
+        self.password_input = (By.XPATH, "//input[@name='password']")  # Tìm trường password
         self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
 
     def open_login_form(self):
