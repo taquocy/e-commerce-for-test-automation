29348c6538884ed27c32b8d0d5c8c9640a124391
phankhacdientrang <Trang93190@donga.edu.vn>
2024-11-06 15:13:50 +0700

    update

--- pages/create_new_product_page.py
+++ pages/create_new_product_page.py
@@ -4,6 +4,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
+
+
 class CreateNewProductPage():
     def __init__(self, driver):
         self.driver = driver
