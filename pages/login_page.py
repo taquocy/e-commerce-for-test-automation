39ef7bf74a33aaf2ef2420f9d0e340121fb05ada
0347508391 <vovanmai3010@gmail.com>
2024-11-06 14:12:32 +0700

    capture elements on login screen

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,6 +12,11 @@ class LoginPage:
         self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
         self.register_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
         self.password_input = (By.NAME, "password")  # Nhap mat khau password
+        self.menu_login = (By.XPATH, "//a[@href='/signin']/button")  # Mở form đăng nhập
+        self.username_input = (By.NAME, "email")  # Tìm trường email
+        self.password_input = (By.NAME, "password")  # Tìm trường mật khẩu
+        self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút đăng nhập
+        self.register_button = (By.XPATH, "//a[@href='/register']/button")  # Nút đăng ký
 
 
     def open_login_form(self):
@@ -27,3 +32,22 @@ class LoginPage:
     # Hàm để nhấn nút login
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+    # Mở form đăng nhập
+    def open_login_form(self):
+        self.driver.find_element(*self.menu_login).click()
+
+    # Nhập email (tên đăng nhập)
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_input).send_keys(username)
+
+    # Nhập mật khẩu
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_input).send_keys(password)
+
+    # Nhấn nút đăng nhập
+    def click_login(self):
+        self.driver.find_element(*self.login_button).click()
+
+    # Mở trang đăng ký
+    def open_register_page(self):
+        self.driver.find_element(*self.register_button).click()
