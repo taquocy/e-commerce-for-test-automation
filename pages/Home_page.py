6d6e4878b982c5fa6ad314ef2b5b8e31cbfb7cef
tronghuy203 <thanhuy571@gmail.com>
2024-11-06 13:51:53 +0700

    update home pages

--- pages/Home_page.py
+++ pages/Home_page.py
@@ -4,6 +4,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
+
+
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
