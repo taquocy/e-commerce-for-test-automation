f90ef3dba87cd90b9f23b5c523593c29c92fc22b
NVMinh <ngominh0141@gmail.com>
2024-11-06 18:14:50 +0700

    NgoVanMinh-Login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import NoSuchElementException
 
 class LoginPage:
     def __init__(self, driver):
@@ -8,12 +8,18 @@ class LoginPage:
 
         # Xác định các phần tử trên trang login
         self.menu_login =  (By.XPATH, "//a[@href='/signin']/button")
+        self.menu_register =  (By.XPATH, "//a[@href='/signup']/button")
         self.username_input = (By.NAME, "email")  # Tìm trường username
         self.password_input = (By.NAME, "password")  # Tìm trường password
         self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
+        self.title = "//h2[normalize-space(text())='%s']"
 
     def open_login_form(self):
         self.driver.find_element(*self.menu_login).click()
+
+    def open_register_form(self):
+        self.driver.find_element(*self.menu_register).click()
+
     # Hàm để nhập tên đăng nhập
     def enter_username(self, username):
         self.driver.find_element(*self.username_input).send_keys(username)
@@ -25,3 +31,24 @@ class LoginPage:
     # Hàm để nhấn nút login
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+    def check_profile_page_display(self):
+        try:
+            # Tìm nút Admin bằng XPath
+            admin_button = self.driver.find_element(By.XPATH, "//button[text()='Profile']")
+
+            # Kiểm tra xem nút Admin có tồn tại không
+            assert admin_button is not None, "Profile button does not exist!"
+            print("Profile button exists!")
+
+        except NoSuchElementException:
+            print("Profile button not found!")
+
+    def get_error_message(self):
+        # Locate the error message element and return its text
+        error_element = self.driver.find_element(By.XPATH, "//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' chakra-alert ')]")  # Replace with actual selector
+        return error_element.text
+    
+    def get_title(self, ttle:str):
+        title_locator = (By.XPATH, self.title % ttle)
+        title_element = self.driver.find_element(*title_locator)
+        return title_element.text
--- tests/test_login.py
+++ tests/test_login.py
@@ -39,6 +39,74 @@ class LoginTest(unittest.TestCase):
         admin_page.check_admin_page_display()
 
 
+    def test_valid_login_with_user_account(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        # Nhập thông tin đăng nhập
+        login_page.enter_username("ngominh011@gmail.com")
+        login_page.enter_password("12345")
+        login_page.click_login()
+
+        login_page.check_profile_page_display()
+
+    def test_user_cannot_login_with_nonexistent_account(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        login_page.enter_username("ngominh01@gmail.com")
+        login_page.enter_password("1235")
+        login_page.click_login()
+
+        # Verify error message
+        error_message = login_page.get_error_message()
+        self.assertEqual(error_message, "The email address was not found.")
+
+    def test_cannot_login_with_valid_email_and_invalid_password(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        login_page.enter_username("ngominh011@gmail.com")
+        login_page.enter_password("1235")
+        login_page.click_login()
+
+        # Verify error message
+        error_message = login_page.get_error_message()
+        self.assertEqual(error_message, "email or password not correct")
+
+    def test_cannot_login_with_invalid_email_and_valid_password(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        login_page.enter_username("ngominh01@gmail.com")
+        login_page.enter_password("12345")
+        login_page.click_login()
+
+        # Verify error message
+        error_message = login_page.get_error_message()
+        self.assertEqual(error_message, "The email address was not found.")
+
+    def test_cannot_login_when_entering_case_sensitive_password(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        login_page.enter_username("ngominh01111@gmail.com")
+        login_page.enter_password("MINH123")
+        login_page.click_login()
+
+        # Verify error message
+        error_message = login_page.get_error_message()
+        self.assertEqual(error_message, "The email address was not found.")
+
+    def test_navigate_to_the_registration_page(self):
+        login_page = LoginPage(self.driver)
+        mess = "Signin"
+        signup = "Signup"
+        login_page.open_login_form()
+        self.assertTrue(login_page.get_title(mess))
+        login_page.open_register_form()
+        self.assertTrue(login_page.get_title(signup))
+
     def tearDown(self):
         self.driver.quit()
 
