2494185984d0a5a140b5cfaf33c0b218fda285ca
typescript <khanh51202@donga.edu.vn>
2024-11-13 14:43:59 +0700

    kk

--- tests/test_login.py
+++ tests/test_login.py
@@ -40,7 +40,35 @@ class LoginTest(unittest.TestCase):
 
     def tearDown(self):
         self.driver.quit()
+class LoginTest1(unittest.TestCase):
 
+    def setUp(self):
+        # Đọc file config.ini
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
+        # Lấy URL trang login từ file config
+        self.login_url = config['app']['login_url']
+
+        # Khởi tạo trình duyệt
+        self.driver = BrowserSetup.get_driver()
+        self.driver.get(self.login_url)  # Sử dụng URL từ file config
+
+    def test_valid_login_with_admin_account(self):
+        login_page = LoginPage(self.driver)
+
+        login_page.open_login_form()
+        # Nhập thông tin đăng nhập bằng tài khoản không tồn tạis
+        login_page.enter_username("khanh@gmail.com")
+        login_page.enter_password("khanh123")
+        login_page.click_login()
+
+        admin_page = AdminPage(self.driver)
+        admin_page.check_admin_page_display()
+
+
+    def tearDown(self):
+        self.driver.quit()
 #  test nhập thông tin bằng 1 tài khoản không tồn tại
 if __name__ == "__main__":
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reports'))
