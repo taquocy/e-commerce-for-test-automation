b672ba77ac45771429f5d4365cd8fd679f81a754
HaiNam1408 <haiinamm1408@gmail.com>
2024-11-27 23:32:26 +0700

    test register

--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -1,5 +1,6 @@
 import configparser
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 class BrowserSetup:
     @staticmethod
@@ -11,8 +12,11 @@ class BrowserSetup:
         # Lấy path driver từ phần cấu hình webdriver
         driver_path = config['webdriver']['driver_path']
 
-        # Tạo instance của WebDriver (Chrome ở đây)
-        driver = webdriver.Chrome(executable_path=driver_path)
+        # Tạo instance của Service với path đến ChromeDriver
+        service = Service(driver_path)
+
+        # Khởi tạo WebDriver (Chrome ở đây) với Service
+        driver = webdriver.Chrome(service=service)
         driver.implicitly_wait(26)
         driver.maximize_window()
         return driver
