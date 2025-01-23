fc82418f7f3aafe6020030dc85cdea1b0e34941c
ZeroT <111341000+PhamTuanKiet002@users.noreply.github.com>
2025-01-23 19:23:56 +0700

    Initial commit

--- pages/create_new_product_page.py
+++ pages/create_new_product_page.py
@@ -9,17 +9,17 @@ class CreateNewProductPage():
         self.driver = driver
 
         # Xác định các phần tử trên trang login
-        self.title_label = (By.XPATH, "//label[@for='field-:r2:']")  # Label cho Title
-        self.title_input = (By.NAME, "title")  # Trường input cho Title
-        self.title_error = (By.XPATH, "//p[contains(text(), 'title is a required field')]")  # Thông báo lỗi cho Title
+        self.title_label = (By.XPATH, "//label[@for='field-:r2:']")  # Label Title
+        self.title_input = (By.NAME, "title")  # Trường input Title
+        self.title_error = (By.XPATH, "//p[contains(text(), 'title is a required field')]")  # Thông báo lỗi Title
 
-        self.description_label = (By.XPATH, "//label[@for='field-:r3:']")  # Label cho Description
-        self.description_input = (By.XPATH, "//textarea[@name='description']")  # Textarea cho Description
+        self.description_label = (By.XPATH, "//label[@for='field-:r3:']")  # Label Description
+        self.description_input = (By.XPATH, "//textarea[@name='description']")  # Textarea Description
 
-        self.price_label = (By.XPATH, "//label[@for='field-:r4:']")  # Label cho Price
-        self.price_input = (By.XPATH, "//input[@name='price']")  # Trường input cho Price
+        self.price_label = (By.XPATH, "//label[@for='field-:r4:']")  # Label Price
+        self.price_input = (By.XPATH, "//input[@name='price']")  # Trường input Price
 
-        self.photos_label = (By.XPATH, "//label[@for='field-:r5:']")  # Label cho Photos
+        self.photos_label = (By.XPATH, "//label[@for='field-:r5:']")  # Label Photos
         self.add_photo_button = (By.XPATH, "//button[text()='Add a Photo']")  # Nút "Add a Photo"
         self.photo_input = (By.XPATH, "//input[@name='photos.0']")
 
@@ -29,15 +29,15 @@ class CreateNewProductPage():
 
 
     def enter_title(self, title):
-        """Nhập Title vào trường Title."""
+        """Nhập Title. """
         self.driver.find_element(*self.title_input).send_keys(title)
 
     def enter_description(self, description):
-        """Nhập Description vào trường Description."""
+        """Nhập Description ."""
         self.driver.find_element(*self.description_input).send_keys(description)
 
     def enter_price(self, price):
-        """Nhập Price vào trường Price."""
+        """Nhập Price ."""
         self.driver.find_element(*self.price_input).send_keys(price)
 
     def click_add_photo(self):
@@ -50,13 +50,12 @@ class CreateNewProductPage():
         self.driver.find_element(*self.photo_input).send_keys(image)
 
     def click_add_product(self):
-        """Nhấn nút 'Add Product' để gửi form."""
+        """Nhấn nút 'Add Product' để đăngđăng."""
         self.driver.find_element(*self.add_product_button).click()
 
     def is_success_message_appeared(self):
         try:
-            # Chờ đợi tối đa 10 giây cho thông báo "Add product successfully" xuất hiện
-            WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 20).until(
                 EC.presence_of_element_located(self.message_create_product_successfully)
             )
             print("Message 'Add product successfully' appeared!")
