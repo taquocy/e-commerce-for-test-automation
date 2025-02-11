d0a29dd1b1083c128b1cbaa755d0b91e0bb79163
ZeroT <111341000+PhamTuanKiet002@users.noreply.github.com>
2025-02-11 08:33:04 +0700

    Update test_admin_new_product.py

--- tests/test_admin_new_product.py
+++ tests/test_admin_new_product.py
@@ -14,22 +14,18 @@ from pages.login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class CreateNewProductTest(unittest.TestCase):
 
     def setUp(self):
         # Đọc file config.ini
         config = configparser.ConfigParser()
         config.read('config.ini')
-
         # Lấy URL trang login từ file config
         self.login_url = config['app']['login_url']
-
         # Khởi tạo trình duyệt
         self.driver = BrowserSetup.get_driver()
         self.driver.get(self.login_url)  # Sử dụng URL từ file config
         login_page = LoginPage(self.driver)
-
         login_page.open_login_form()
         # Nhập thông tin đăng nhập
         login_page.enter_username("superadmin@gmail.com")
@@ -46,12 +42,9 @@ class CreateNewProductTest(unittest.TestCase):
         create_new_product_page.click_add_photo()
         create_new_product_page.enter_image_url("https://images.unsplash.com/photo-1726839662758-e3b5da59b0fb?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
         create_new_product_page.click_add_product()
-
-
+        
     def tearDown(self):
         self.driver.quit()
-
-
 if __name__ == "__main__":
     # unittest.main()
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reports'))
