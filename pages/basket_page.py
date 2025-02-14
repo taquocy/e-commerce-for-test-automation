b1ee63000494840e66aeae97f555097da4035379
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-02-15 01:54:05 +0700

    test python

---
+++ pages/basket_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+class BasketPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.remove_button = (By.XPATH, "//*[@id='content']/div//button")
+        self.basket_button = (
+            By.XPATH, "//*[@id='root']/nav/div[2]/a[@href='/basket']")
+        self.basket_button = (By.XPATH, "//a[@href='/basket']")
+        self.item_image = (By.XPATH, "//*[@id='content']/div//a/img")
+        self.total_price = (By.XPATH, "//p[contains(text(), 'Total:')]")
+        
+    def open_basket(self):
+        self.driver.find_element(*self.basket_button).click()
+
+    def remove_item_from_basket(self):
+        
+        self.driver.find_element(*self.remove_button).click()
+
+
+    def get_item_image_src(self):
+        return self.driver.find_element(*self.item_image).get_attribute("src")
+
+    def get_total_price(self):
+        return self.driver.find_element(*self.total_price).text
--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -2,23 +2,21 @@ import configparser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-
 class BrowserSetup:
     @staticmethod
     def get_driver():
-        # Đọc file config.ini
+
         config = configparser.ConfigParser()
-        config.read('config.ini')  # Đọc file config.ini từ thư mục gốc
+        config.read('config.ini')
 
-        # Lấy path driver từ phần cấu hình webdriver
         driver_path = config['webdriver']['driver_path']
+        service = Service(driver_path)
 
-        # Tạo instance của WebDriver (Chrome ở đây)          
-        service = Service(driver_path)  # Create a Service object with the path to chromedriver
-        driver = webdriver.Chrome(service=service)
+        options = webdriver.ChromeOptions()
 
-        # Tạo instance của WebDriver (Chrome ở đây)
-        # driver = webdriver.Chrome(executable_path=driver_path)
+        options.binary_location = config['chromium']['binary_location']
+
+        driver = webdriver.Chrome(service=service, options=options)
         driver.implicitly_wait(26)
         driver.maximize_window()
         return driver
