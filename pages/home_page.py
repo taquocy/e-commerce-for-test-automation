4f481542db1108ed9cee3d7d80a19110bd1802d1
Cường Trần Phước <tranphuoccuong07@gmail.com>
2025-03-04 16:49:54 +0700

    Cuong_home

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,34 +1,25 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-
-# Khởi tạo trình duyệt (ví dụ: Chrome)
-driver = webdriver.Chrome()
-
-# Truy cập vào trang web
-driver.get("https://e-commerce-for-testing.onrender.com")
-
-# Đăng nhập vào tài khoản admin
-email_input = driver.find_element(By.XPATH, "//input[@type='email']")
-password_input = driver.find_element(By.XPATH, "//input[@type='password']")
-login_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Login')]")
-
-email_input.send_keys("superadmin@gmail.com")
-password_input.send_keys("admin123")
-login_button.click()
-
-# Chờ đợi một chút để trang Home load xong
-time.sleep(5)
-
-# Lấy tất cả các phần tử trên trang Home bằng XPath
-elements = driver.find_elements(By.XPATH, "//*")
 
 
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        # Xác định các phần tử trên trang login
+        self.menu_login =  (By.XPATH, "//a[@href='/signin']/button")
+        self.username_input = (By.NAME, "email")  # Tìm trường username
+        self.password_input = (By.NAME, "password")  # Tìm trường password
+        self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
 
+    def open_login_form(self):
+        self.driver.find_element(*self.menu_login).click()
+    # Hàm để nhập tên đăng nhập
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_input).send_keys(username)
 
-# In ra thông tin của các phần tử
-for element in elements:
-    print(f"Tag: {element.tag_name}, Text: {element.text}, ID: {element.get_attribute('id')}, Class: {element.get_attribute('class')}")
+    # Hàm để nhập mật khẩu
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_input).send_keys(password)
 
-# Đóng trình duyệt
-driver.quit()
+    # Hàm để nhấn nút login
+    def click_login(self):
+        self.driver.find_element(*self.login_button).click()
