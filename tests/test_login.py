83c8785abd0600d50ecc186aee9c98fc26e636b3
khangle3901 <ldkdx2906@gmail.com>
2025-05-14 17:37:51 +0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,48 +1,41 @@
 import unittest
 import configparser
-
 import HtmlTestRunner
 import sys
 import os
 
-# Thêm đường dẫn đến thư mục gốc vào sys.path
+# Add project root to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pages.login_page import LoginPage
 from pages.admin_page import AdminPage
 from utils.browser_setup import BrowserSetup
 
-
 class LoginTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        # Read config.ini
+        cls.config = configparser.ConfigParser()
+        cls.config.read('config.ini')
+
+        # Initialize browser
+        cls.driver = BrowserSetup.get_driver()
+
     def setUp(self):
-        # Đọc file config.ini
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-
-        # Lấy URL trang login từ file config
-        self.login_url = config['app']['login_url']
-
-        # Khởi tạo trình duyệt
-        self.driver = BrowserSetup.get_driver()
-        self.driver.get(self.login_url)  # Sử dụng URL từ file config
+        # Get login URL from config
+        self.login_url = self.config['app']['login_url']
+        self.driver.get(self.login_url)
 
     def test_valid_login_with_admin_account(self):
         login_page = LoginPage(self.driver)
-
-        login_page.open_login_form()
-        # Nhập thông tin đăng nhập
-        login_page.enter_username("superadmin@gmail.com")
-        login_page.enter_password("admin123")
-        login_page.click_login()
+        login_page.login("superadmin@gmail.com", "admin123")
 
         admin_page = AdminPage(self.driver)
         admin_page.check_admin_page_display()
 
-
-    def tearDown(self):
-        self.driver.quit()
-
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == "__main__":
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reports'))
-
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reports', report_name='LoginTestReport', report_title='Login Test Report'))
