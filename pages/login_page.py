4c91dda7e75c82e689bf10540fa4a8a582cac910
khangle3901 <ldkdx2906@gmail.com>
2025-05-14 17:31:00 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,26 +1,37 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
         self.driver = driver
-        # Xác định các phần tử trên trang login
-        self.menu_login =  (By.XPATH, "//a[@href='/signin']/button")
-        self.username_input = (By.NAME, "email")  # Tìm trường username
-        self.password_input = (By.NAME, "password")  # Tìm trường password
-        self.login_button = (By.XPATH, "//button[@type='submit']")  # Nút submit
+        self.wait = WebDriverWait(self.driver, 10)  # Explicit wait for better stability
+
+        # Locators
+        self.menu_login = (By.XPATH, "//a[@href='/signin']/button")
+        self.username_input = (By.NAME, "email")
+        self.password_input = (By.NAME, "password")
+        self.login_button = (By.XPATH, "//button[@type='submit']")
 
     def open_login_form(self):
-        self.driver.find_element(*self.menu_login).click()
+        self.wait.until(EC.element_to_be_clickable(self.menu_login)).click()
 
-    # Hàm để nhập tên đăng nhập
-    def enter_username(self, username):
-        self.driver.find_element(*self.username_input).send_keys(username)
+    def enter_username(self, username: str):
+        username_field = self.wait.until(EC.visibility_of_element_located(self.username_input))
+        username_field.clear()
+        username_field.send_keys(username)
 
-    # Hàm để nhập mật khẩu
-    def enter_password(self, password):
-        self.driver.find_element(*self.password_input).send_keys(password)
+    def enter_password(self, password: str):
+        password_field = self.wait.until(EC.visibility_of_element_located(self.password_input))
+        password_field.clear()
+        password_field.send_keys(password)
 
-    # Hàm để nhấn nút login
     def click_login(self):
-        self.driver.find_element(*self.login_button).click()
+        self.wait.until(EC.element_to_be_clickable(self.login_button)).click()
+
+    def login(self, username: str, password: str):
+        self.open_login_form()
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
